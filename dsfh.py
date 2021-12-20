f05d469e82a105a412ffc4b4b8ce3d833dc2a494
Thenafi <nafio62346@niter.edu.bd>
2021-12-21 00:11:39 +0600

    ok

--- dsfh.py
+++ dsfh.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import time
-from termcolor import colored
+
 
 options = Options()
 options.headless = True
@@ -10,25 +10,29 @@ driver = webdriver.Chrome(options=options, executable_path=r'chromedriver.exe')
 
 driver.get("https://www.daraz.com.bd/")
 time.sleep(0.1)
-print(colored("into website", "red"))
+
 search = driver.find_element_by_id("q")
 search.click()
+
+
+
 time.sleep(0.1)
-search.send_keys("Walton 64GB Class 10/U3 Micro SDHC/SDXC Memory Card with Adapter")
+search.send_keys("Walton 64GB")
 time.sleep(5)
 
 
 searchbuttonclick = driver.find_element_by_class_name("search-box__button--1oH7")
 searchbuttonclick.click()
-time.sleep(0.1)
-print(colored("search done", "red"))
+
+
 
 clicksdcard=driver.find_element_by_link_text("Walton 64GB Class 10/U3 Micro SDHC/SDXC Memory Card with Adapter")
 clicksdcard.click()
 
 
+
 price = driver.find_element_by_css_selector(".pdp-price.pdp-price_type_normal.pdp-price_color_orange.pdp-price_size_xl")
-pricetext= price.text
+
 print(price.text)
 
 
